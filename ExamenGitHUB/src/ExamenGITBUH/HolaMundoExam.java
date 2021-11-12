b2dc7e3f5bb62a679dfc5d889e1d0a68f455ff54
Usuario <Usuario@aula8-01>
2021-11-12 09:49:01 +0100

    Updated

---
+++ ExamenGitHUB/src/ExamenGITBUH/HolaMundoExam.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ExamenGITBUH;
+
+/**
+ * @author Usuario
+ *
+ */
+public class HolaMundoExam {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
